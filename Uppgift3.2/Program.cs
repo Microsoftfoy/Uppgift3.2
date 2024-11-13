36184cf2f32f82fb37dfe592c1082b8b36d3ca57
ercr07001 <ercr07001@E-PW05CGR0>
2024-11-13 12:38:19 +0100

    Add project files.

---
+++ Uppgift3.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut ur gymmnasiet?");
+            Console.WriteLine("Svara j för Ja och n för Nej");
+            string x = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            int år = int.Parse(Console.ReadLine());
+
+            if(x == "j" && år <= 22)
+            {
+                Console.WriteLine("Vi vill anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Vi vill inte anställa dig"); Environment.Exit(0);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
